5206f20289ea225235857f809558e432cac65715
sousa <tahirsousa@gmail.com>
2015-05-01 02:56:47 -0500

    Several additions to setup.py including runnable test class

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-from distutils.core import setup
+from distutils.core import setup, Command
+from codecs import open
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys
+        import subprocess
+        errno = subprocess.call([sys.executable, 'SimilarityCalculator/test/unit_tests_load_neighbors.py'])
+        raise SystemExit(errno)
 
 setup(
     name='SimilarityCalculator',
@@ -6,11 +26,16 @@ setup(
     version='0.1',
     description='Python package that computes similarity between two items based on their tag relevance scores and '
                 'writes to file the list of items along with their corresponding top item neighbors',
+    long_description=long_description,
     author='Tahir Sousa',
     author_email='tahirsousa@gmail.com',
     url='https://github.com/SMLtahir/SimilarityCalculator',
     download_url='https://github.com/SMLtahir/SimilarityCalculator/tarball/0.1',
+    maintainer='Tahir Sousa',
+    maintainer_email='tahirsousa@gmail.com',
+    license='GNU General Public License',
     keywords=['similarity', 'neighbor', 'neighborhood'],
+    cmdclass={'test': PyTest},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
