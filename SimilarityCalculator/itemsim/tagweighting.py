0a2a159edafdd1815c435c5310c49738db0e66c0
sousa <tahirsousa@gmail.com>
2015-05-02 10:04:41 -0500

    Weighted/Unweighted structure made more intuitive in tagweighting

--- SimilarityCalculator/itemsim/tagweighting.py
+++ SimilarityCalculator/itemsim/tagweighting.py
@@ -6,34 +6,29 @@ class TagWeighting:
         pass
 
 
-import math
-
-DEFAULT_FREQ = 10
-LOG_SHIFT = 1
-
-
 class PopularityIdfTagWeighting(TagWeighting):
-    def __init__(self, tag_rel, pop_transform=math.log, idf_transform=math.log, weighted=True,
-                 weights_dictionary_path=None):
+    def __init__(self, weighted=False, weights_dictionary_path=None):
         self.weighted = weighted
         self.weightsDict = {}
-        if weights_dictionary_path is not None:
-            first_line = True
-            for line in open(weights_dictionary_path, 'r'):
-                if first_line:
-                    first_line = False
-                    continue
-                values = line.strip().split('\t')
-                tag = values[0]
-                weight = int(values[1])
-                self.weightsDict[tag] = weight
+        if not self.weighted:
+            print "All tags will be equally weighted."
+        else:
+            if weights_dictionary_path is not None:
+                first_line = True
+                for line in open(weights_dictionary_path, 'r'):
+                    if first_line:
+                        first_line = False
+                        continue
+                    values = line.strip().split('\t')
+                    tag = values[0]
+                    weight = int(values[1])
+                    self.weightsDict[tag] = weight
+            else:
+                print "Error! Please input a path to valid Tag Weights file to continue."
+                exit()
 
     def get_weight(self, tag):
-        if not self.weighted:
-            if len(self.weightsDict) == 0:
-                return 1
-            else:
-                return self.weightsDict[tag]
+        if self.weighted:
+            return self.weightsDict[tag]
         else:
-            print "Added functionality to be added here."
             return 1
