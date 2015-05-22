83f2dcd693e1caaf3650ee0705f81b465f1b455a
sousa <tahirsousa@gmail.com>
2015-05-22 03:37:51 -0500

    Sample file for running SimilarityCalculator as a function

---
+++ SimilarityCalculator/data/samples/runAsFunction.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.insert(0, 'directoryWhere_SimilarityCalculator/load_neighbors.py_IsLocated')
+# example: sys.path.insert(0, '/home/user/SimilarityCalculator')
+from SimilarityCalculator import load_neighbors
+
+load_neighbors.run()
+print "The run was a Success!!"
+
